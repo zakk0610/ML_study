8c67a3bc36ca76061626175ad14831a35998dadd
zakk <zakk0610@gmail>
2016-09-13 01:13:33 +0800

    finish softmax

--- cs231n/assignment1/cs231n/classifiers/softmax.py
+++ cs231n/assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,25 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  #print W.shape #(3073, 10)
+  #print X.shape #(500, 3073)
+  #print y.shape #(500,)
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  for i in xrange(num_train):
+        scores = X[i].dot(W)
+        scores = scores - np.max(scores)  # Numeric stability.
+        exp_scores = np.exp(scores)
+        #print prob
+        loss += -scores[y[i]] + np.log(np.sum(exp_scores))
+        for j in xrange(num_classes):
+            dW[:, j] += exp_scores[j] / np.sum(exp_scores) * X[i,:]
+            if j == y[i]:
+                dW[:, j] += -X[i,:]
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+  dW /= num_train
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +71,30 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  #print W.shape #(3073, 10)
+  #print X.shape #(500, 3073)
+  #print y.shape #(500,)
+
+  num_train = X.shape[0]
+  scores = np.dot(X, W)  # 500 x 10
+
+  max_val = np.max(scores, axis=1, keepdims=True) # 500
+  scores = scores - max_val  # Numeric stability.
+  exp_scores = np.exp(scores)
+  # loss_i = -f_yi + log(summation of e^fj)
+  loss = np.sum(-1 * scores[y]) + np.sum(np.log(np.sum(exp_scores, axis=1)))
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+  # grads
+  # tmp = (e^Wjxj / (summation all of e^fxj),  i is index of training data
+  #dw[Wyi] = -xi + tmp x xi = (-1 + tmp) x xi
+  #dw[Wyj] = tmp x xi
+
+  tmp = exp_scores/np.sum(exp_scores, axis=1, keepdims=True)
+  tmp[range(num_train), y[range(num_train)]] += -1
+  dW = np.dot(X.T, tmp)
+  dW /= num_train
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
